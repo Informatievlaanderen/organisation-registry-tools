fa9a52c8adc369b4ed8b9a5547e36e9cfe0f0d33
Quinten Van Assche <quinten.vanassche@greenstack.be>
2022-05-23 11:19:19 +0200

    feat: or-1017 add csv upload to update vlimpercode enddate

---
+++ OrganisationRegistryTools.OR-1017.VlimpersKeyImport/UpdateOrganisationKeyRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace OrganisationRegistryTools.OR_1017.VlimpersKeyImport;
+
+public class UpdateOrganisationKeyRequest
+{
+    public Guid OrganisationKeyId { get; set; }
+    public Guid KeyTypeId { get; set; }
+    public string KeyValue { get; set; } = null!;
+    public DateTime? ValidFrom { get; set; }
+    public DateTime? ValidTo { get; set; }
+}
