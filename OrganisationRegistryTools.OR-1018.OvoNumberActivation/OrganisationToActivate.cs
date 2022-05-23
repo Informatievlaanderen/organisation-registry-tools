f2d08e156c122c3b54d13bdfa741c765a0bd914f
Quinten Van Assche <quinten.vanassche@greenstack.be>
2022-05-23 12:55:19 +0200

    feat: or-1018 add csv upload ovoNumber activate

---
+++ OrganisationRegistryTools.OR-1018.OvoNumberActivation/OrganisationToActivate.cs
@@ -0,0 +1,9 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace OrganisationRegistryTools.VlimpersFlagImport;
+
+public class OrganisationToActivate
+{
+    [Index(0)] public string OvoNumber { get; set; }
+    [Format("d/MM/yyyy")] [Index(1)] public DateTime StartDate { get; set; }
+}
