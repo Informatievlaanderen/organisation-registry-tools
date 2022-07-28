4e4f630e449831d27c21e0b6c6eb4f381d10b4e4
W0dan <W0dan.git@gmail.com>
2022-07-28 14:11:54 +0200

    feat: or-962 remove testapp for business because postman

--- ApiTestAppForBusiness/ApiTestAppForBusiness/Model/Group.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ApiTestAppForBusiness.Model;
-
-public class Group
-{
-    public string Name { get; set; }= null!;
-    public IEnumerable<Action> Actions { get; set; } = null!;
-}
--- ApiTestAppForBusiness/ApiTestAppForBusiness/Model/Parameter.cs
+++
@@ -1,9 +0,0 @@
-namespace ApiTestAppForBusiness.Model;
-
-public class Parameter
-{
-    public string Type { get; set; } = null!; // Route, Query or Body
-    public string DataType { get; set; } = null!; // string, guid, object
-    public string? Name { get; set; }
-    public string? Structure { get; set; }
-}
